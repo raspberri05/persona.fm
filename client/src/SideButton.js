bec959b488a90330e2d896046bb001f6664a7194
Vedant Singhania <vedant.singhania@gmail.com>
2022-04-05 18:01:23 -0700

    cleaned up unneeded code

--- client/src/SideButton.js
+++ client/src/SideButton.js
@@ -2,7 +2,6 @@ import React from 'react'
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App.js'
 
 class SideButton extends React.Component {
 

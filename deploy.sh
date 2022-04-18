01aa8c62be874e9cee428588624368e4d2c7ff6d
Vedant Singhania <vedant.singhania@gmail.com>
2022-04-18 10:36:56 -0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 cd client
 
+sed 's@http://localhost:3001/login@/login@g' src/components/Login.js > temp.js
+
+cp src/components/temp.js src/components/Login.js
+
+rm src/components/temp.js
+
 npm run build
 
 mv build ../../tunestats_build
@@ -21,3 +27,13 @@ git commit -m "$commitMessage"
 git push
 
 git push heroku main
+
+cd ../tunestats/client
+
+sed 's@/login@http://localhost:3001/login@g' src/components/Login.js > temp.js
+
+cp src/components/temp.js src/components/Login.js
+
+rm src/components/temp.js
+
+

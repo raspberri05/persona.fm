0d254488ba4bcb9daf2af26b8c0cdc929af7ba4f
Vedant Singhania <vedant.singhania@gmail.com>
2024-07-02 12:46:00 -0700

    upgrade to next

---
+++ tunestats/app/dashboard/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+export default function Page() {
+    const [auth, setAuth] = useState(false)
+
+    function getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g
+        var q = window.location.hash.substring(1);
+        while (e = r.exec(q)) {
+            //@ts-expect-error
+          hashParams[e[1]] = decodeURIComponent(e[2]);
+        }
+        return hashParams;
+      }
+
+      useEffect(() => {
+        const access_token = getHashParams().access_token;
+        console.log(access_token)
+        if (access_token) {
+          setAuth(true)
+        }
+        else {
+            window.location.href = "http://localhost:3000/"
+        }
+      }, []);
+
+    
+    return (
+        <div className="text-center">
+        <br/>
+        <p className="text-2xl">View your top songs, artists, and recently played songs!</p>
+        <p className="text-2xl">Please login with your preferred music account using the button above to begin</p>
+        <br/>
+        </div>
+    );
+}
---
+++ tunestats/app/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+export default function Page() {
+
+  function login() {
+    window.location.href = "http://localhost:3001/login";
+  }
+  return (
+    <div className="text-center">
+      <p className="text-4xl">Tunestats</p>
+      <br/>
+      <button className="btn btn-primary" onClick={login}>Login with Spotify</button>
+      <br/>
+      <br/>
+      <p className="text-2xl">View your top songs, artists, and recently played songs!</p>
+      <p className="text-2xl">Please login with your preferred music account using the button above to begin</p>
+      <br/>
+    </div>
+  );
+}

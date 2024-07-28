d426be957fb6500990d5f5d19eef035701ff6f68
Vedant Singhania <vedant.singhania@gmail.com>
2024-07-27 17:25:26 -0700

    update drizzle schema

--- app/api/db/schema.ts
+++ app/api/db/schema.ts
@@ -1,27 +1,20 @@
-import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users_table", {
+export const users = pgTable("users", {
     id: serial("id").primaryKey(),
-    name: text("name").notNull(),
-    age: integer("age").notNull(),
-    email: text("email").notNull().unique(),
+    username: text("username").notNull(),
 });
 
-export const postsTable = pgTable("posts_table", {
+export const personas = pgTable("persona", {
     id: serial("id").primaryKey(),
-    title: text("title").notNull(),
-    content: text("content").notNull(),
-    userId: integer("user_id")
+    week: text("week").notNull(),
+    persona: text("persona").notNull(),
+    user_id: integer("user_id")
         .notNull()
-        .references(() => usersTable.id, { onDelete: "cascade" }),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at")
-        .notNull()
-        .$onUpdate(() => new Date()),
+        .references(() => users.id, { onDelete: "cascade" }),
 });
 
-export type InsertUser = typeof usersTable.$inferInsert;
-export type SelectUser = typeof usersTable.$inferSelect;
+export type InsertUser = typeof users.$inferInsert;
+export type SelectUser = typeof users.$inferSelect;
 
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
+export type InsertPersona = typeof personas.$inferInsert;
--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,7 +1,5 @@
 import { db } from "../db";
-import { InsertUser, usersTable } from "../db/schema";
 
 export async function GET(request: Request) {
-    await db.insert(usersTable);
     return new Response("Hello, world!");
 }

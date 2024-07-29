c300680c954f0e76d2b6aea236f2a7f943e14c6b
Vedant Singhania <vedant.singhania@gmail.com>
2024-07-29 14:19:18 -0700

    display data

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import axios from "axios";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Page() {
     const hasFetched = useRef(false);
+    const [persona, setPersona] = useState<any>([]);
 
     function getMain() {
         return axios
             .get("/api/main")
             .then((res) => {
                 const data = JSON.parse(res.data);
-                console.log(data.vibe);
+                setPersona(data);
+                console.log(data);
             })
             .catch((err) => {
                 console.error(err);
@@ -32,8 +34,20 @@ export default function Page() {
 
     return (
         <div className="container mx-auto px-2 py-2">
-            <h1 className="text-3xl font-bold">Home</h1>
-            <p className="text-lg">Welcome to the home page!</p>
+            {persona.length === 0 && (
+                <span className="loading loading-spinner loading-md"></span>
+            )}
+            {persona.length !== 0 && (
+                <div>
+                    <h1 className="text-3xl font-bold">{persona.vibe}</h1>
+                    <br />
+                    <p className="text-lg">{`${persona.mainstream?.percent}% mainstream`}</p>
+                    <p className="text-lg">{persona.mainstream?.description}</p>
+                    <br />
+                    <p className="text-lg">{`${persona.energetic?.percent}% energetic`}</p>
+                    <p className="text-lg">{persona.energetic?.description}</p>
+                </div>
+            )}
         </div>
     );
 }

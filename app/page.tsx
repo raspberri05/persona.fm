fa5b648604c70b548bdaa75c3181184bf9139fc1
Naya Singhania <naya.singhania@gmail.com>
2024-10-14 19:26:50 -0700

    fix build issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-import { redirect, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { Suspense, useEffect } from "react";
 import { createUser } from "./actions";
 
 export default function Page() {
-    const searchParams = useSearchParams();
-    const redirected = searchParams.get("redirected") === "true";
-
-    useEffect(() => {
-        // Call createUser when the component mounts
+    function Check() {
+        const searchParams = useSearchParams();
+        const redirected = searchParams.get("redirected") === "true";
         if (redirected) {
             createUser()
                 .then((user) => {
@@ -20,28 +18,35 @@ export default function Page() {
                     console.error("Error creating user:", error);
                 });
         }
-    }, [redirected]);
+        return (
+            <div className="text-center">
+                {!redirected && (
+                    <>
+                        <br />
+                        <p className="text-6xl">Welcome to Persona.fm!</p>
+                        <br />
+                        <p className="text-3xl">
+                            Your AI-Generated Last.fm music persona
+                        </p>
+                        <br />
+                        <p className="text-xl">
+                            <a href="/login" className="underline">
+                                Log In
+                            </a>{" "}
+                            to get started
+                        </p>
+                    </>
+                )}
+                {redirected && (
+                    <p>Successfully logged in! Redirecting you now</p>
+                )}
+            </div>
+        );
+    }
 
     return (
-        <div className="text-center">
-            {!redirected && (
-                <>
-                    <br />
-                    <p className="text-6xl">Welcome to Persona.fm!</p>
-                    <br />
-                    <p className="text-3xl">
-                        Your AI-Generated Last.fm music persona
-                    </p>
-                    <br />
-                    <p className="text-xl">
-                        <a href="/login" className="underline">
-                            Log In
-                        </a>{" "}
-                        to get started
-                    </p>
-                </>
-            )}
-            {redirected && <p>Successfully logged in! Redirecting you now</p>}
-        </div>
+        <Suspense>
+            <Check />
+        </Suspense>
     );
 }

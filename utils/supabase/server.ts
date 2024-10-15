1e897ca8ab2c0ac8d8e9968fb1b81f694fc8018e
Naya Singhania <naya.singhania@gmail.com>
2024-10-14 20:10:00 -0700

    null assertion fixes

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -5,8 +5,8 @@ export function createClient() {
     const cookieStore = cookies();
 
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
         {
             cookies: {
                 getAll() {

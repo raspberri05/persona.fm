bed2fcd09e2abf619bac6614560ee8f153f22fa7
Naya Singhania <naya.singhania@gmail.com>
2024-10-16 23:13:37 -0700

    fixes 2.0

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,7 +5,9 @@ const withNextra = nextra({
   themeConfig: './theme.config.jsx'
 })
  
-export default withNextra()
+export default withNextra({
+  output: 'export'
+})
  
 // If you have other Next.js configurations, you can pass them as the parameter:
 // export default withNextra({ /* other next.js config */ })

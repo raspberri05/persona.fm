91b59d26bb9179994771c0708a9d95cdeebdb3c7
Naya Singhania <naya.singhania@gmail.com>
2024-10-18 11:13:31 -0700

    Update profile-card.tsx
    Signed-off-by: Naya Singhania <naya.singhania@gmail.com>

--- application/app/components/molecules/profile-card.tsx
+++ application/app/components/molecules/profile-card.tsx
@@ -4,7 +4,7 @@ import { getData } from "@/app/api/actions/settings/actions";
 import { useEffect, useState } from "react";
 import Card from "@/app/components/atoms/card";
 import { User } from "@/utils/types";
-import { Image } from 'next/image';
+import Image from 'next/image';
 
 export default function ProfileCard() {
     const [displayName, setDisplayName] = useState("");

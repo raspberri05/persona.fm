13fb9b2f85e2d2c62de5ee12eec2882277d2515e
Naya Singhania <naya.singhania@gmail.com>
2024-10-19 12:37:07 -0700

    update footer

--- application/app/components/organisms/footer.tsx
+++ application/app/components/organisms/footer.tsx
@@ -1,47 +1,46 @@
-import LinkButton from "@/app/components/molecules/link-button";
+import Image from "next/image";
 
 export default function Footer() {
     return (
-        <footer className="footer footer-center bg-base-200 text-base-content rounded p-10 bg-transparent">
-            <nav className="grid grid-flow-row gap-2 mb-[-0.5rem]">
-                <div className="grid grid-flow-col gap-4">
-                    <LinkButton variant="ghost" text="About" href="/" />
-                    <LinkButton
-                        variant="ghost"
-                        text="Contact"
-                        href="/contact"
-                    />
-                </div>
-                <div className="grid grid-flow-col gap-4">
-                    <LinkButton
-                        variant="ghost"
-                        text="Privacy"
-                        href="/privacy"
-                    />
-                    <LinkButton variant="ghost" text="Terms" href="/terms" />
-                </div>
-            </nav>
-            <nav>
-                <div className="grid grid-flow-col gap-4 mb-[-1rem]">
-                    <a href="https://github.com/raspberri05/persona.fm">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            fill="currentColor"
-                            className="bi bi-github"
-                            viewBox="0 0 16 16"
-                        >
-                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-                        </svg>
-                    </a>
-                </div>
-            </nav>
+        <footer className="footer p-10">
             <aside>
-                <p className="mb-[-1rem]">
-                    Copyright {new Date().getFullYear()} Naya Singhania
+                <Image
+                    src="icon.svg"
+                    alt="persona.fm logo"
+                    width="40"
+                    height="40"
+                />
+                <p>
+                    Persona.fm
+                    <br />
+                    Copyright 2024 Naya Singhania
                 </p>
             </aside>
+            <nav>
+                <h6 className="footer-title">Learn More</h6>
+                <a className="link link-hover" href="/">About</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Community</h6>
+                <a
+                    className="link link-hover"
+                    href="https://docs.personafm.com"
+                >
+                    Documentation
+                </a>
+                <a className="link link-hover" href="/contact">
+                    Contact
+                </a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Legal</h6>
+                <a className="link link-hover" href="/terms">
+                    Terms of use
+                </a>
+                <a className="link link-hover" href="/privacy">
+                    Privacy policy
+                </a>
+            </nav>
         </footer>
     );
 }

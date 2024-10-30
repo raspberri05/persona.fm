01e3816bbf3deb66d0bf68b7b5afd7831e623c9d
Naya Singhania <naya.singhania@gmail.com>
2024-10-30 13:30:36 -0700

    fix robots.txt error

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -45,7 +45,8 @@ export async function updateSession(request: NextRequest) {
         request.nextUrl.pathname !== "/terms" &&
         request.nextUrl.pathname !== "/privacy" &&
         request.nextUrl.pathname !== "/contact" &&
-        request.nextUrl.pathname !== "/sitemap.xml"
+        request.nextUrl.pathname !== "/sitemap.xml" && 
+        request.nextUrl.pathname !== "/robots.txt"
     ) {
         // no user, potentially respond by redirecting the user to the login page
         const url = request.nextUrl.clone();

51d5d38fbc33534efc339e1ea6d9a097ab506d04
Naya Singhania <naya.singhania@gmail.com>
2024-10-31 11:34:11 -0700

    finish basic ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return (
-    <div>hello</div>
-  )
+    return (
+        <div className="h-[80vh] flex items-center justify-center text-center">
+            <div className="space-y-2">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                    Discover Your Music Persona
+                </h1>
+                <p className="max-w-[700px] md:text-xl">
+                    Transform your Last.fm listening history into a unique
+                    musical personality. Get insights about your music taste and
+                    find your music soulmates.
+                </p>
+                <Button className="rounded-xl h-12 px-8 font-medium" size="lg">
+                    Log with Last.fm
+                </Button>{" "}
+            </div>
+        </div>
+    );
 }
